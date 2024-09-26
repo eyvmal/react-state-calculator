d2d98d6443ace5471594750927d1ff7695ffb361
eyvmal <eyvind.malde@gmail.com>
2024-09-26 15:31:08 +0200

    core and extensions

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,71 @@
 import "./App.css"
+import {useEffect, useState} from "react";
 
 function App() {
+  // Arrays of the buttons used in the calculator
+  const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', 'clear', 'ANS'];
+  const operators = ['+', '-', '*', '/'];
+  
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [secondNumber, setSecondNumber] = useState('0');
+  const [operator, setOperator] = useState('+');
+  const [answer, setAnswer] = useState(0);
+  const [prevAnswer, setPrevAnswer] = useState('0');
 
+  // Method to handle click on the numpads
+  const handleClick = (setter, number, buttonLabel) => {
+    if (buttonLabel === '.' && number.includes(buttonLabel)) return // Ignore if the number already has a decimal point
+    if (buttonLabel === 'clear') {
+      setter('0')
+    } else if (buttonLabel === 'ANS') {
+      setter(`${prevAnswer}`)
+    } else {
+      setter(number === '0' && buttonLabel !== '.' ? `${buttonLabel}` : number + `${buttonLabel}`) // Remove leading zeros if conditions are met
+    }
+  }
+
+  // Auto update the answer when either the numbers or the operator is changed
+  useEffect(() => {
+    setAnswer(parseFloat(eval(`${firstNumber} ${operator} ${secondNumber}`).toFixed(2)))
+  }, [firstNumber, operator, secondNumber]);
+
+  const saveAnswer = () => {
+    setPrevAnswer(`${answer}`)
+  }
+  
   return (
     <div className="calculator">
       <div className="panel">
-        <p>0</p>
+        <p>{firstNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {buttons.map((buttonLabel, index) => (
+              <button key={index} onClick={() => handleClick(setFirstNumber, firstNumber, buttonLabel)}>{buttonLabel}</button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
         <p>+</p>
         <div className="numbers">
-          <button>+</button>
-          <button>-</button>
-          <button>*</button>
-          <button>÷</button>
+          {operators.map((buttonLabel, index) => (
+              <button key={index} onClick={() => handleClick(setOperator(buttonLabel))}>{buttonLabel}</button>
+          ))}
         </div>
       </div>
 
       <div className="panel">
-        <p>0</p>
+        <p>{secondNumber}</p>
         <div className="numbers">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>Clear</button>
+          {buttons.map((buttonLabel, index) => (
+              <button key={index} onClick={() => handleClick(setSecondNumber, secondNumber, buttonLabel)}>{buttonLabel}</button>
+          ))}
         </div>
       </div>
       <div className="panel answer">
-        <p>0</p>
+        <p>{answer}</p>
         <div>
           <button>=</button>
+          <button onClick={() => saveAnswer()}>Save</button>
         </div>
       </div>
     </div>
